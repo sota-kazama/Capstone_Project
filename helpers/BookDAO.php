d5bde13223386dbf9e5b6d4f21ca849ebda7554c
sota-kazama <220344304+sota-kazama@users.noreply.github.com>
2025-10-20 20:38:04 +0900

    書籍検索ページの実装(内容は完成)

--- helpers/BookDAO.php
+++ helpers/BookDAO.php
@@ -1,14 +1,44 @@
 <?php
 require_once 'DAO.php';
 
-class Book
+class Books
 {
     public string $book_code;  //書籍コード
     public string $book_name;  //書籍名
     public string $sakusya;    //作者名
-    public string $shuppan;    //出版社名
-    public string $created_at; //登録日
+    public string $syuppan;    //出版社名
+    public string $created_ad; //登録日
     public string $update_at;  //更新日
 }
 
+class BookDAO{
+    public function searchBooks(string $keyword)
+{
+    $dbh = DAO::get_db_connect();
+
+    $sql = "SELECT * FROM books
+            WHERE book_code LIKE ? OR book_name LIKE ? OR
+                sakusya LIKE ? OR syuppan LIKE ?
+            ORDER BY book_code DESC"; //
+
+    $stmt = $dbh->prepare($sql);
+
+    $searchKeyword = '%' . $keyword . '%';
+    $stmt->bindValue(1, $searchKeyword, PDO::PARAM_STR);
+    $stmt->bindValue(2, $searchKeyword, PDO::PARAM_STR);
+    $stmt->bindValue(3, $searchKeyword, PDO::PARAM_STR);
+    $stmt->bindValue(4, $searchKeyword, PDO::PARAM_STR);
+
+    // SQLを実行する
+    $stmt->execute();
+
+    $data = [];
+    while ($row = $stmt->fetchObject('Books')) {
+        $data[] = $row;
+    }
+
+    // 取得した結果を返す
+    return $data;
+
+}}
 ?>
