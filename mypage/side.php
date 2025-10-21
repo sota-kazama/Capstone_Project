2f197a31ff575613b9b1bff85d47a9bd085ed7e2
sota-kazama <220344304+sota-kazama@users.noreply.github.com>
2025-10-21 20:13:28 +0900

    レイアウトの微調整

--- mypage/side.php
+++ mypage/side.php
@@ -1,30 +1,34 @@
+<?php
+$current = basename($_SERVER['PHP_SELF']);
+?>
+
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px; height: 1617px;">
     <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        </a>
+    </a>
     <ul class="nav nav-pills flex-column mb-auto">
         <li>
-            <a href="./mypage.php" class="nav-link link-body-emphasis" >
+            <a href="./mypage.php" class="nav-link <?= $current === 'mypage.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-square"></i>
                 マイページトップ
             </a>
         </li>
         <li>
-            <a href="setting.php" class="nav-link link-body-emphasis">
+            <a href="setting.php" class="nav-link <?= $current === 'setting.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-book"></i>
                 アカウント設定
             </a>
         </li>
         <li>
-            <a href="goal.php" class="nav-link link-body-emphasis">
+            <a href="goal.php" class="nav-link <?= $current === 'goal.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-book"></i>
                 目標設定
             </a>
         </li>
         <li>
-            <a href="results.php" class="nav-link link-body-emphasis">
+            <a href="results.php" class="nav-link <?= $current === 'results.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-book"></i>
                 成果登録
             </a>
         </li>
     </ul>
-    </div>
+</div>
--- template/side.php
+++ template/side.php
@@ -1,18 +1,22 @@
+<?php
+$current = basename($_SERVER['PHP_SELF']);
+?>
+
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px; height: 1617px;">
     <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        </a>
+    </a>
     <ul class="nav nav-pills flex-column mb-auto">
         <li>
-            <a href="question.php" class="nav-link link-body-emphasis" >
+            <a href="question.php" class="nav-link <?= $current === 'question.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-square"></i>
                 問題
             </a>
         </li>
         <li>
-            <a href="book.php" class="nav-link link-body-emphasis">
+            <a href="book.php" class="nav-link <?= $current === 'book.php' ? 'active' : 'link-body-emphasis' ?>">
                 <i class="bi bi-book"></i>
                 書籍検索
             </a>
         </li>
     </ul>
-    </div>
+</div>
