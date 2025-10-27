d327ca1bd4c7180b376d4ba3a42ca837d961f339
sota-kazama <220344304+sota-kazama@users.noreply.github.com>
2025-10-27 18:53:09 +0900

    ログイン処理の実装及びアカウント作成時のパスワードのハッシュ化の実装

--- login.php
+++ login.php
@@ -39,66 +39,61 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8" />
+        <link href="css/BaseDesignData.css" rel="stylesheet" />
+        <link rel="stylesheet" href="css/LoginDesign.css" />
+        <title>ログイン</title>
+    </head>
 
-<head>
-    <meta charset="utf-8" />
-    <link href="css/BaseDesignData.css" rel="stylesheet" />
-    <link rel="stylesheet" href="css/LoginDesign.css">
-    <title>ログイン</title>
-
-</head>
-
-<body>
+    <body>
         <?php include 'template/header2.php'; ?>
-<form action="" method="POST">
-    <table id="LoginTable" class="box">
-        <tr>
-            <th colspan="2">ログイン</th>
-        </tr>
-        <tr>
-            <td>メールアドレス</td>
-            <td>
-                <input type="email" name="mail_address" required autofocus />
-            </td>
-        </tr>
-        <tr>
-            <td>パスワード</td>
-            <td>
-                <input type="password" name="pass_word" required autofocus/>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <input type="submit" value="ログイン" />
-            </td>
-        </tr>
-        <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($errs)): ?>
-        <tr>
-            <td colspan="2">
-                <?php foreach ($errs as $e): ?>
-                    <span style="color:red"><?= htmlspecialchars($e, ENT_QUOTES, 'UTF-8') ?></span><br>
-                <?php endforeach; ?>
-            </td>
-        </tr>
-        <?php endif; ?>
+        <form action="" method="POST">
+            <table id="LoginTable" class="box">
+                <tr>
+                    <th colspan="2">ログイン</th>
+                </tr>
+                <tr>
+                    <td>メールアドレス</td>
+                    <td>
+                        <input type="email" name="mail_address" required autofocus />
+                    </td>
+                </tr>
+                <tr>
+                    <td>パスワード</td>
+                    <td>
+                        <input type="password" name="pass_word" required autofocus />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="ログイン" />
+                    </td>
+                </tr>
+                <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($errs)): ?>
+                <tr>
+                    <td colspan="2">
+                        <?php foreach ($errs as $e): ?>
+                        <span style="color: red"><?= htmlspecialchars($e, ENT_QUOTES, 'UTF-8') ?></span><br />
+                        <?php endforeach; ?>
+                    </td>
+                </tr>
+                <?php endif; ?>
+            </table>
+        </form>
 
-    </table>
-</form>
-                    
-    <table class="box">
-        <tr>
-            <th>初めての利用の方</th>
-        </tr>
-        <tr>
-            <td>ログインするには会員登録が必要です。</td>
-        </tr>
-        <tr>
-            <td><a href="signup.php">新規会員登録はこちら</a></td>
-        </tr>
-    </table>
-</body>
+        <table class="box">
+            <tr>
+                <th>初めての利用の方</th>
+            </tr>
+            <tr>
+                <td>ログインするには会員登録が必要です。</td>
+            </tr>
+            <tr>
+                <td><a href="signup.php">新規会員登録はこちら</a></td>
+            </tr>
+        </table>
+    </body>
 </html>
-
