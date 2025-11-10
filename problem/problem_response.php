da8eb6aa336b446e906678f8b39d7c24c819cd05
24yn0121 <220701592+24yn0121@users.noreply.github.com>
2025-11-10 20:15:31 +0900

    変更

--- problem/problem_response.php
+++ problem/problem_response.php
@@ -1,26 +1,3 @@
-<?php
-require_once '../helpers/ProblemDAO.php'; //問題情報用
-require_once '../helpers/MemberDAO.php';  //ユーザー毎の正誤率把握のため
-require_once '../ProblemErrataDAO.php';   //問題正誤情報
-
-// セッションを開始する
-session_start();
-
-//ログイン情報の取得
-if (!empty($_SESSION['member'])) {
-    $member = $_SESSION['member'];
-}
-
-// ログイン中の会員データを取得
-$member = $_SESSION['member'];
-
-//問題情報を取得
-
-//正誤情報の取得
-
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -39,7 +16,7 @@ $member = $_SESSION['member'];
 
     <head>
         <!--こっちのheadを変更しない-->
-        <title></title>
+        <title>メインページ</title>
     </head>
 
     <body>
@@ -48,38 +25,9 @@ $member = $_SESSION['member'];
 
             <main class="main-content">
                 <!--ここに記載する-->
-                <h1>問題回答</h1>
-                <!--
-
-                取得した情報から問題文を表示
-
-                ループ文で回答ボタンを選択肢の数だけ表示
-                選択肢はボタン形式で番号が問題の選択肢番号と一致するように設定
-                -->
-
-
-
-
-                <!--
-                if(回答ボタンが押されたか){
-                    if(ログインかどうか){
-                        if(正解かどうか){
-                            正解と表示
-                            正解をカウント
-                        }{else
-                            不正解と表示
-                            不正解をカウント
-                        }
-                    }else {
-                        if(正解かどうか){
-                            正解と表示
-                        }{else
-                            不正解と表示
-                    }}
-                正解と解説を表示
--->
-
-                <!--次の問題を表示するボタン-->
+                <h1>問題復習</h1>
+                <p>ここに、メインとなるページの内容が生成されます。</p>
+                <div style="height: 1500px; background-color: #f8f9fa">長いコンテンツの例</div>
             </main>
         </div>
 
