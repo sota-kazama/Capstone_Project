c6cf762f5c3f597c0144769ae95ab03015f38757
24YN0125 <220344048+24yn0125@users.noreply.github.com>
2025-12-02 20:00:41 +0900

    ログインボタンデザイン変更

--- template/header.php
+++ template/header.php
@@ -20,32 +20,33 @@ if (is_array($member) && isset($member['user_name'])) {
 ?>
 <!DOCTYPE html>
 <html lang="jp">
-    <head>
-        <meta charset="UTF-8" />
-        <link href="../css/BaseDesignData.css" rel="stylesheet" />
-    </head>
-    <body>
-        <header>
-            <?php include "template/hamburger.php"; ?>
-            <a href="index.php">
-                <img src="images/icon2.png" alt="サイトのロゴ" />
-            </a>
 
-            <?php if ($userName !== null) : ?>
+<head>
+    <meta charset="UTF-8" />
+    <link href="../css/BaseDesignData.css" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+        <?php include "template/hamburger.php"; ?>
+        <a href="index.php">
+            <img src="images/icon2.png" alt="サイトのロゴ" />
+        </a>
+
+        <?php if ($userName !== null) : ?>
             <p id="logout">
                 <?= htmlspecialchars($userName) ?> さん
                 <a href="logout.php" class="logout-btn">ログアウト</a>
             </p>
 
-            <?php else : ?>
+        <?php else : ?>
             <form action="login.php" method="post">
                 <p id="login">
-                    <input type="submit" value="ログイン" />
+                    <input type="submit" value="ログイン" class="login-btn1">
                 </p>
-            </form>
             <?php endif; ?>
+    </header>
+    <hr />
+</body>
 
-        </header>
-        <hr />
-    </body>
 </html>
