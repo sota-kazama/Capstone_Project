3d97747aefac66669eca62c4c83b7b89c0b7599e
24YN0125 <220344048+24yn0125@users.noreply.github.com>
2026-02-05 20:42:27 +0900

    --利用者マニュアル--

--- template/footer.php
+++ template/footer.php
@@ -10,7 +10,7 @@
             <!-- <a href="<?= BASE_URL ?>/footer/termsOfUse.php">利用規約</a>
             <a href="<?= BASE_URL ?>/footer/policy.php">ポリシー</a> -->
             <!--<a href="<?= BASE_URL ?>/footer/sitemap.php">サイトマップ</a> -->
-            <a href="docs/利用者マニュアル.docx" download>利用者マニュアルダウンロード</a>
+            <a href="/Capstone_Project/docs/利用者マニュアル.pdf" target="_blank">利用者マニュアル</a>
         </div>
     </footer>
 </html>
